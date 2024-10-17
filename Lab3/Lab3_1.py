1addbb7313572151b719c9ea95e647e2708dfb01
kacper0276 <kacper0276@op.pl>
2024-10-17 14:03:43 +0200

    Lab 3 - Ex 1

--- Lab3/Lab3_1.py
+++ Lab3/Lab3_1.py
@@ -1,9 +1,37 @@
 from PIL import Image
 import numpy as np
 
-def rysuj_ramki_szare(w,h,grub):
-    print(w)
+# Zad 1
 
-def rysuj_ramki_szare(w,h,grub):
-    print(w)
+def rysuj_ramki_szare(w, h, grub):
+    t = (h, w)
+    tab = np.ones(t, np.uint8) * 255
 
+    ile_ramek = min(w, h) // (2 * grub)
+    szarosc_krok = 255 // (ile_ramek + 1)
+
+    for i in range(ile_ramek):
+        color = i * szarosc_krok
+        tab[i * grub:h - i * grub, i * grub:w - i * grub] = color
+
+    return Image.fromarray(tab)
+
+def rysuj_pasy_pionowe_szare(w, h, grub):
+    t = (h, w)
+    tab = np.ones(t, np.uint8) * 255
+
+    ile = int(w / grub)
+    szarosc_krok = 255 // (ile + 1)
+
+    for i in range(ile):
+        color = i * szarosc_krok
+        tab[:, i * grub:(i + 1) * grub] = color
+
+    return Image.fromarray(tab)
+
+
+ramka = rysuj_ramki_szare(300, 200, 5)
+ramka.show()
+
+pasy = rysuj_pasy_pionowe_szare(300, 200, 5)
+pasy.show()
