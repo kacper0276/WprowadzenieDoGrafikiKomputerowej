3c9b5ab8aa85b29f713397b9a56c9ddb36f04e9f
kacper0276 <kacper0276@op.pl>
2024-10-24 14:36:16 +0200

    Lab 4 - Ex 3

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -32,4 +32,44 @@ im1 = Image.merge('RGB', (im_r, im_g, im_b))
 
 diff = ImageChops.difference(im, im1)
 
-print(diff)
+
+plt.figure(figsize=(16, 16))
+plt.subplot(2,2,1)
+plt.imshow(im)
+plt.axis('off')
+plt.subplot(2,2,2)
+plt.imshow(im1)
+plt.axis('off')
+plt.subplot(2,2,3)
+plt.imshow(diff)
+plt.axis('off')
+plt.subplots_adjust(wspace=0.05, hspace=0.05)
+# plt.savefig('fig1.png')
+# plt.show()
+
+# Zadanie 3
+
+r, g, b = im.split()
+
+img2 = Image.merge("RGB", (b, g, r))
+
+# img2.save('im2.jpg')
+# img2.save('im2.png')
+
+img2Jpg = Image.open('im2.jpg')
+img2Png = Image.open('im2.png')
+
+diff2 = ImageChops.difference(img2Jpg, img2Png)
+
+plt.figure(figsize=(16, 16))
+plt.subplot(2,2,1)
+plt.imshow(img2Jpg)
+plt.axis('off')
+plt.subplot(2,2,2)
+plt.imshow(img2Png)
+plt.axis('off')
+plt.subplot(2,2,3)
+plt.imshow(diff2)
+plt.axis('off')
+plt.subplots_adjust(wspace=0.05, hspace=0.05)
+# plt.savefig('fig2.png')
