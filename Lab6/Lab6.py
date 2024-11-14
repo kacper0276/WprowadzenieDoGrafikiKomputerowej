19de9e346c74142d4d37924982fae46aaa863434
kacper0276 <kacper0276@op.pl>
2024-11-14 19:31:43 +0100

    Lab 6 - Ex 3

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -16,9 +16,9 @@ def wstaw_inicjaly(obraz, inicjaly, m, n, kolor):
                 obraz_copy.putpixel((m + x, n + y), kolor)
     return obraz_copy
 
-m = obraz.width - inicjaly.width
-n = obraz.height - inicjaly.height
-obraz1 = wstaw_inicjaly(obraz, inicjaly, m, n, (255, 0, 0))
+ma = obraz.width - inicjaly.width
+na = obraz.height - inicjaly.height
+obraz1 = wstaw_inicjaly(obraz, inicjaly, ma, na, (255, 0, 0))
 # obraz1.show()
 
 def wstaw_inicjaly_maska(obraz, inicjaly, m, n):
@@ -31,15 +31,50 @@ def wstaw_inicjaly_maska(obraz, inicjaly, m, n):
                 obraz_copy.putpixel((m + x, n + y), (255 - r, 255 - g, 255 - b))
     return obraz_copy
 
-m = (obraz.width - inicjaly.width) // 2
-n = (obraz.height - inicjaly.height) // 2
-obraz2 = wstaw_inicjaly_maska(obraz, inicjaly, m, n)
+mb = (obraz.width - inicjaly.width) // 2
+nb = (obraz.height - inicjaly.height) // 2
+obraz2 = wstaw_inicjaly_maska(obraz, inicjaly, mb, nb)
 # obraz2.show()
 
 # Zad 3
 def wstaw_inicjaly_load(obraz, inicjaly, m, n, kolor):
-    return None
+    obraz_copy = obraz.copy()
+    obraz_pixels = obraz_copy.load()
+    initials_pixels = inicjaly.load()
+    width, height = inicjaly.size
+    for x in range(width):
+        for y in range(height):
+            if initials_pixels[x, y] == 0:
+                obraz_pixels[m + x, n + y] = kolor
+    return obraz_copy
 
-def wstaw_inicjaly_maska(obraz, inicjaly, m, n, x, y, z):
-    return None
+def wstaw_inicjaly_maska_load(obraz, inicjaly, m, n):
+    obraz_copy = obraz.copy()
+    obraz_pixels = obraz_copy.load()
+    initials_pixels = inicjaly.load()
+    width, height = inicjaly.size
+    for x in range(width):
+        for y in range(height):
+            if initials_pixels[x, y] == 0:
+                r, g, b = obraz_pixels[m + x, n + y]
+                obraz_pixels[m + x, n + y] = (255 - r, 255 - g, 255 - b)
+    return obraz_copy
+
+obraz3 = wstaw_inicjaly_load(obraz, inicjaly, ma, na, (255, 0, 0))
+obraz4 = wstaw_inicjaly_maska_load(obraz, inicjaly, mb, nb)
+
+plt.figure(figsize=(16, 16))
+plt.subplot(1, 2, 1)
+plt.imshow(obraz3)
+plt.title("Obraz 3")
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(obraz4)
+plt.title("Obraz 4")
+plt.axis('off')
+
+plt.title('Zadanie 3')
+# plt.show()
+# plt.savefig('fig1.png')
 
