aba1f984b066dc089d28464c721ac43ee837a97c
kacper0276 <kacper0276@op.pl>
2024-11-14 14:19:20 +0200

    Lab 6 - Ex 2, 3 - Init

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -6,3 +6,17 @@ import matplotlib.pyplot as plt
 obraz = Image.open('obraz.png')
 inicjaly = Image.open('inicjaly.png')
 
+# Zad 2
+def wstaw_inicjaly(obraz, inicjaly, m, n, kolor):
+    return None
+
+def wstaw_inicjaly_maska(obraz, inicjaly, m, n):
+    return None
+
+# Zad 3
+def wstaw_inicjaly_load(obraz, inicjaly, m, n, kolor):
+    return None
+
+def wstaw_inicjaly_maska(obraz, inicjaly, m, n, x, y, z):
+    return None
+
