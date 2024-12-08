164ac918e439529795ffa1c1608ae954ea2f2e33
kacper0276 <kacper0276@op.pl>
2024-12-08 20:38:47 +0100

    Lab 8 - Ex 2

--- Lab8/Lab8_1.py
+++ Lab8/Lab8_1.py
@@ -4,6 +4,7 @@ from PIL import ImageChops
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Zad 1
 
 def filtruj(obraz, kernel, scale):
     img_array = np.array(obraz.convert("L"))
@@ -42,4 +43,46 @@ scale = 9
 
 wynik = filtruj(obraz, kernel, scale)
 
-wynik.show()
+# wynik.show()
+
+# Zad 2
+def statystyki_obrazu(obr):
+    obr_array = np.array(obr)
+    return {
+        "Średnia": np.mean(obr_array),
+        "Odchylenie standardowe": np.std(obr_array),
+        "Minimum": np.min(obr_array),
+        "Maksimum": np.max(obr_array),
+    }
+
+blur_pil = obraz.filter(ImageFilter.BLUR)
+
+kernel_blur = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+scale_blur = 9
+blur_custom = filtruj(obraz, kernel_blur, scale_blur)
+
+roznica = ImageChops.difference(blur_pil.convert("L"), blur_custom.convert("L"))
+
+statystyki = statystyki_obrazu(roznica)
+
+fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+
+axs[0].imshow(obraz, cmap="gray")
+axs[0].set_title("Obraz oryginalny")
+axs[0].axis("off")
+
+axs[1].imshow(blur_pil, cmap="gray")
+axs[1].set_title("ImageFilter.BLUR")
+axs[1].axis("off")
+
+axs[2].imshow(blur_custom, cmap="gray")
+axs[2].set_title("Filtr własny")
+axs[2].axis("off")
+
+axs[3].imshow(roznica, cmap="gray")
+axs[3].set_title("Różnica obrazów")
+axs[3].axis("off")
+
+plt.tight_layout()
+plt.savefig("fig1.png")
+plt.show()
