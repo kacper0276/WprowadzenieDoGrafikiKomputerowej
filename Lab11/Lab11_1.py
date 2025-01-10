726e759977fd74ee9947c430916f6a39c86d0f0b
kacper0276 <kacper0276@op.pl>
2025-01-10 10:11:01 +0100

    Lab 11 - Ex 1

--- Lab11/Lab11_1.py
+++ Lab11/Lab11_1.py
@@ -3,3 +3,12 @@ import numpy as np
 from PIL import ImageOps, ImageChops
 import matplotlib.pyplot as plt
 from PIL import ImageStat as stat
+
+# Zad 1
+obraz1 = Image.open("messi.jpg")
+obraz2 = Image.open("olsztyn.jpg").resize(obraz1.size)
+
+alpha = 2 / (2 + 3)
+mix = Image.blend(obraz1, obraz2, alpha=alpha)
+
+mix.show()
