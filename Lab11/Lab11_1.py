0ad43f958c2ccc1c3d5c7c3e69f9dff4bdb26bcf
kacper0276 <kacper0276@op.pl>
2025-01-10 10:13:41 +0100

    Lab 11 - Ex 2

--- Lab11/Lab11_1.py
+++ Lab11/Lab11_1.py
@@ -12,3 +12,11 @@ alpha = 2 / (2 + 3)
 mix = Image.blend(obraz1, obraz2, alpha=alpha)
 
 mix.show()
+
+# Zad 2
+
+left, top, right, bottom = 100, 100, 300, 300
+fragment = obraz1.crop((left, top, right, bottom))
+
+obraz2.paste(fragment, (50, 50))
+obraz2.show()
