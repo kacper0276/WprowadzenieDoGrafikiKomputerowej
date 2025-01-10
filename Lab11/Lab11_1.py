de0b874af4235a6521069010299bc484760d2b08
kacper0276 <kacper0276@op.pl>
2025-01-10 10:16:50 +0100

    Lab 11 - Upload font

--- Lab11/Lab11_1.py
+++ Lab11/Lab11_1.py
@@ -14,9 +14,11 @@ mix = Image.blend(obraz1, obraz2, alpha=alpha)
 mix.show()
 
 # Zad 2
-
 left, top, right, bottom = 100, 100, 300, 300
 fragment = obraz1.crop((left, top, right, bottom))
 
 obraz2.paste(fragment, (50, 50))
 obraz2.show()
+
+# Zad 3
+text = "Jedi używa Mocy do zdobywania wiedzy i obrony, nigdy do ataku"
