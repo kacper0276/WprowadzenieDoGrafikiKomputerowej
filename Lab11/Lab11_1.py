c83206ee178b28b9ab105a0af84f3663743af76a
kacper0276 <kacper0276@op.pl>
2025-01-10 10:05:47 +0100

    Lab 11 - Init files

---
+++ Lab11/Lab11_1.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import numpy as np
+from PIL import ImageOps, ImageChops
+import matplotlib.pyplot as plt
+from PIL import ImageStat as stat
---
+++ Lab11/Lab11_2.py
@@ -0,0 +1,6 @@
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import numpy as np
+from PIL import ImageOps
+import matplotlib.pyplot as plt
+from PIL import ImageFilter
+import math
