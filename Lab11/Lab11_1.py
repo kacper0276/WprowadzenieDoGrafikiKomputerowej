760ff2f1fc16bbb7868e55739d7d035f72006c4b
kacper0276 <kacper0276@op.pl>
2025-01-14 16:11:25 +0100

    Lab 11_2 - Ex 1

--- Lab11/Lab11_1.py
+++ Lab11/Lab11_1.py
@@ -1,8 +1,9 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 import numpy as np
-from PIL import ImageOps, ImageChops
+from PIL import ImageOps
 import matplotlib.pyplot as plt
-from PIL import ImageStat as stat
+from PIL import ImageFilter
+import math
 
 # Zad 1
 obraz1 = Image.open("messi.jpg")
@@ -11,14 +12,21 @@ obraz2 = Image.open("olsztyn.jpg").resize(obraz1.size)
 alpha = 2 / (2 + 3)
 mix = Image.blend(obraz1, obraz2, alpha=alpha)
 
-mix.show()
+# mix.show()
 
 # Zad 2
 left, top, right, bottom = 100, 100, 300, 300
 fragment = obraz1.crop((left, top, right, bottom))
 
 obraz2.paste(fragment, (50, 50))
-obraz2.show()
+# obraz2.show()
 
 # Zad 3
+obrazEx3 = Image.open('Shrek_Fiona.png')
 text = "Jedi używa Mocy do zdobywania wiedzy i obrony, nigdy do ataku"
+fnt = ImageFont.truetype("ttf/DejaVuSansDisplay.ttf", 30)
+d = ImageDraw.Draw(obrazEx3)
+color = ImageColor.getrgb("#7fff00")
+d.text((40,40), text, font=fnt, fill=color, align ="left")
+
+obrazEx3.show()
--- Lab11/Lab11_2.py
+++ Lab11/Lab11_2.py
@@ -1,6 +1,64 @@
-from PIL import Image, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageDraw, ImageFont, ImageColor, ImageChops
 import numpy as np
 from PIL import ImageOps
 import matplotlib.pyplot as plt
 from PIL import ImageFilter
 import math
+
+# Zad 1
+image1 = Image.open('obraz.png')
+
+image2 = image1.convert('YCbCr')
+
+image2.save('obraz2.jpg')
+
+def rgb_to_ycbcr(image):
+    img_np = np.array(image)
+
+    R = img_np[:, :, 0]
+    G = img_np[:, :, 1]
+    B = img_np[:, :, 2]
+
+    Y = 16 + (64.738 * R + 129.057 * G + 25.064 * B) / 255.
+    Cb = 128 + (-37.945 * R - 74.494 * G + 112.439 * B) / 255.
+    Cr = 128 + (112.439 * R - 94.154 * G - 18.285 * B) / 255.
+
+    Y = np.round(Y).astype(np.uint8)
+    Cb = np.round(Cb).astype(np.uint8)
+    Cr = np.round(Cr).astype(np.uint8)
+
+    img_ycbcr = np.stack([Y, Cb, Cr], axis=-1)
+
+    return img_ycbcr
+
+image3_np = rgb_to_ycbcr(image1)
+
+image3 = Image.fromarray(image3_np, mode='YCbCr')
+
+image3.save('obraz3.jpg')
+
+def compare_images(img1, img2):
+    np_img1 = np.array(img1)
+    np_img2 = np.array(img2)
+
+    diff = np.abs(np_img1 - np_img2)
+    total_diff = np.sum(diff)
+
+    different_pixels = np.count_nonzero(diff)
+
+    return total_diff, different_pixels
+
+difference = ImageChops.difference(image2, image3)
+
+difference.save('difference.jpg')
+
+histogram = difference.histogram()
+
+plt.figure(figsize=(10, 6))
+plt.bar(range(len(histogram)), histogram, color='blue', alpha=0.7)
+plt.title("Histogram różnic między obrazami")
+plt.xlabel("Wartość różnicy pikseli (0-255)")
+plt.ylabel("Liczba pikseli")
+plt.grid(axis='y', linestyle='--', alpha=0.6)
+plt.show()
+plt.savefig('histogram.jpg')
